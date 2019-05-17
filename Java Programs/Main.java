8adc33f0f715df0c76098f525f19c55fa37f7141
15101998 <50664771+15101998@users.noreply.github.com>
2019-05-17 13:58:33 +0530

    Delete Main.java

--- Java Programs/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main {
-public static void main(String[] args) {
-String a;
-Scanner in = new Scanner(System.in);
-a = in.nextLine();
-System.out.println("Welcome to "+a);
-}
-}
